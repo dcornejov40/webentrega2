50a893f6716b61658f9501aa42570397bda9f4e4
DCORNEJOV40 <dcornejov@gmail.com>
2023-07-31 21:56:58 -0400

    ingreso product

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import CartWidget from './components/CartWidget/CartWidget';
-
+import Product from './components/Product/Product';
 
 function App() {
   return (
     <div className="App">
      <NavBar />
       <CartWidget />
+      <Product />
     </div>
   );
 }
